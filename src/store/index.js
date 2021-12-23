c5d1c20134e619e383ede27a412f270ea4dda57d
Sealman <castle2668@gmail.com>
2021-12-23 18:57:49 +0800

    Attach Payload to Actions Object

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,11 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'INCREMENT') {
     return { counter: state.counter + 1 };
   }
+
+  if (action.type === 'INCREASE') {
+    return { counter: state.counter + action.amount };
+  }
+
   if (action.type === 'DECREMENT') {
     return { counter: state.counter - 1 };
   }
