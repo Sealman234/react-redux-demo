ae91cdf4cea30fc39426db3be6a6f57e630d1bb7
Sealman <castle2668@gmail.com>
2021-12-27 11:21:00 +0800

    Create State Slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,31 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
+
+createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    // 現在不用帶 type 了，所以這邊不會用到 action
+    increment(state) {
+      state.counter++; // Redux Toolkit 會自動把它變成 immutable 的方式
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    // 加上 action 因為需要其他參數
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 // Mutations, set initial State
 const counterReducer = (state = initialState, action) => {
